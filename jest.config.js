868c40348d31151b8b4e5071eac50641f80179c8
Martin Denyer <martin.denyer@engagecrm.co.uk>
2018-06-25 14:51:51 +0100

    added jest linting and code coverage config. ignore src folder on publish

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/src/*.js",
+    "!**/src/index.js",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
 };
